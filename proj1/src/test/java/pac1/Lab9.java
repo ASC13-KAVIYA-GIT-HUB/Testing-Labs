9576ae16f371f95ff0c0ae3814e267e6b53f45b2
kaviya.hr <kaviya.hr@192.168.2.78>
2025-10-16 15:48:46 +0530

    lab completed

---
+++ proj1/src/test/java/pac1/Lab9.java
@@ -0,0 +1,23 @@
+package pac1;
+ 
+public class Lab9 {
+	
+	    // Credentials
+	    private String username = "username123";
+	    private String password = "pass123";
+	    // Getters
+	    public String getUsername() {
+	        return username;
+	    }
+	    public String getPassword() {
+	        return password;
+	    }
+	    // Setters (if needed for flexibility)
+	    public void setUsername(String username) {
+	        this.username = username;
+	    }
+	    public void setPassword(String password) {
+	        this.password = password;
+	    }
+	}
+ 
