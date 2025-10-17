862db007877df193dc8266e37677ff08279a05ec
ASC13-KAVIYA-GIT-HUB <mik4sa.eren@gmail.com>
2025-10-17 15:10:19 +0530

    Update Lab11

--- proj1/src/test/java/pac1/Lab11_login_pagefactory.java
+++ proj1/src/test/java/pac1/Lab11_login_pagefactory.java
@@ -1,38 +1,40 @@
-//PAGE FACTORY
-package pac1;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+package Pack1;
+
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class Lab11_login_pagefactory {
-	WebDriver driver;
-	@FindBy(name="username")
-		WebElement	uname;
-	//By uname=By.name("username");
-//By pword=By.name("password");
-@FindBy(name="password")
-WebElement	pword;
-//By submitbutton=By.xpath("//button[@type='submit']");
-@FindBy(xpath="//button[@type='submit']")
-WebElement	submitbutton;
-//By dashboard=By.xpath("//h6[text()='Dashboard']");
-@FindBy(xpath="//h6[text()='Dashboard']")
-WebElement	dashboard;
-public void enterusername(String username)
-{
-	uname.sendKeys(username);
-}
-public void enterpassword(String password)
-{
-	pword.sendKeys(password);
-}
-public void clickonsubmit()
-{
-	submitbutton.click();
-}
-public boolean dashboarddisplayed()
-{
-	return dashboard.isDisplayed();
-}
-}
- 
+    WebDriver driver;
+
+    public HomePageFactory(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    @FindBy(linkText = "Desktops")
+    public WebElement desktopsTab;
+
+    @FindBy(linkText = "Mac (1)")
+    public WebElement macLink;
+
+    @FindBy(xpath = "//h2[text()='Mac']")
+    public WebElement macHeading;
+
+    @FindBy(id = "input-sort")
+    public WebElement sortDropdown;
+
+    @FindBy(xpath = "//button[@onclick[contains(., 'cart.add')]]")
+    public WebElement addToCartButton;
+
+    @FindBy(name = "search")
+    public WebElement searchBox;
+
+    @FindBy(css = "button[class='btn btn-default btn-lg']")
+    public WebElement searchButton;
+
+    @FindBy(name = "description")
+    public WebElement descriptionCheckbox;
+
+    @FindBy(css = "input[type='button'][value='Search']")
+    public WebElement searchAgainButton;
